61d7164b153f315f3ef9922a4cd90e1be7740dc9
ChingWenWen <box789987@gmail.com>
2024-05-22 15:49:21 +0800

    新增彈跳視窗

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,16 @@
+// 新增中間彈出視窗js
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+}
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
+
+
 // Tabs
 function openLink(evt, linkName) {
     var i, x, tablinks;
